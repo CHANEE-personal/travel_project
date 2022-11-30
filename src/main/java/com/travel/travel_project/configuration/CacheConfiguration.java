8a827c66cf5c8d581de4d3f042de9e6523e61690
CHO CHAN HEE <cksgml4424@gmail.com>
2022-11-30 13:34:26 +0900

    redis config 추가 #27

--- src/main/java/com/travel/travel_project/configuration/CacheConfiguration.java
+++ src/main/java/com/travel/travel_project/configuration/CacheConfiguration.java
@@ -1,39 +1,53 @@
 package com.travel.travel_project.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 import static java.util.List.of;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
+
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    RedisConnectionFactory connectionFactory;
+
     /**
      * <pre>
-     * 1. MethodName : cacheManager
+     * 1. MethodName : redisCacheManager
      * 2. ClassName  : CacheConfiguration.java
      * 3. Comment    : api cache 설정
-     * 4. 작성자       : CHO
-     * 5. 작성일       : 2021. 02. 09.
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2021. 02. 09.
      * </pre>
      */
     @Bean
-    public CacheManager cacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(of(
-                new ConcurrentMapCache("user"),
-                new ConcurrentMapCache("travel"),
-                new ConcurrentMapCache("group"),
-                new ConcurrentMapCache("group_user"),
-                new ConcurrentMapCache("post"),
-                new ConcurrentMapCache("review"),
-                new ConcurrentMapCache("notice"),
-                new ConcurrentMapCache("faq"),
-                new ConcurrentMapCache("common")));
-        return cacheManager;
+    public CacheManager redisCacheManager(RedisConnectionFactory cf) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(3L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf).cacheDefaults(redisCacheConfiguration).build();
     }
 }
