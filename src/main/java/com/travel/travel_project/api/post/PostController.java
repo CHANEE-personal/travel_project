6b8a24a7e70968e2fedbb12d87180852766132c8
CHO CHAN HEE <cksgml4424@gmail.com>
2022-12-12 23:18:14 +0900

    게시글 관련 controller 추가 #4

---
+++ src/main/java/com/travel/travel_project/api/post/PostController.java
@@ -0,0 +1,188 @@
+package com.travel.travel_project.api.post;
+
+import com.travel.travel_project.common.Page;
+import com.travel.travel_project.common.SearchCommon;
+import com.travel.travel_project.domain.post.PostDTO;
+import com.travel.travel_project.domain.post.PostEntity;
+import com.travel.travel_project.exception.TravelException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import javax.validation.Valid;
+import java.rmi.ServerError;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.travel.travel_project.exception.ApiExceptionType.NOT_FOUND_POST;
+import static java.lang.Math.ceil;
+
+@RestController
+@RequestMapping("/api/post")
+@Api(tags = "게시글 관련 API")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+    private final SearchCommon searchCommon;
+
+    /**
+     * <pre>
+     * 1. MethodName : findPostList
+     * 2. ClassName  : PostController.java
+     * 3. Comment    : 게시글 리스트 조회
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 12. 12.
+     * </pre>
+     */
+    @ApiOperation(value = "게시글 리스트 조회", notes = "게시글 리스트를 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "게시글 리스트 조회 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @GetMapping(value = "/lists")
+    public Map<String, Object> findPostList(@RequestParam(required = false) Map<String, Object> paramMap, Page page) {
+        Map<String, Object> postMap = new HashMap<>();
+
+        int postCount = this.postService.findPostCount(searchCommon.searchCommon(page, paramMap));
+        List<PostDTO> postList = new ArrayList<>();
+
+        if (postCount > 0) {
+            postList = this.postService.findPostList(searchCommon.searchCommon(page, paramMap));
+        }
+
+        // 리스트 수
+        postMap.put("pageSize", page.getSize());
+        // 전체 페이지 수
+        postMap.put("perPageListCnt", ceil((double) postCount / page.getSize()));
+        // 전체 아이템 수
+        postMap.put("postListCnt", postCount);
+
+        postMap.put("postList", postList);
+
+        return postMap;
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : findOnePost
+     * 2. ClassName  : PostController.java
+     * 3. Comment    : 게시글 상세 조회
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 12. 12.
+     * </pre>
+     */
+    @ApiOperation(value = "게시글 상세 조회", notes = "게시글 상세 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "게시글 상세 조회 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @GetMapping(value = "/{idx}")
+    public PostDTO findOnePost(@PathVariable Long idx) {
+        return postService.findOnePost(idx);
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : insertPost
+     * 2. ClassName  : PostController.java
+     * 3. Comment    : 게시글 등록
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 12. 12.
+     * </pre>
+     */
+    @ApiOperation(value = "게시글 등록", notes = "게시글 등록한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "게시글 등록 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @PostMapping
+    public PostDTO insertPost(@Valid @RequestBody PostEntity postEntity) {
+        return postService.insertPost(postEntity);
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : updateNotice
+     * 2. ClassName  : PostController.java
+     * 3. Comment    : 게시글 수정
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 12. 12.
+     * </pre>
+     */
+    @ApiOperation(value = "게시글 수정", notes = "게시글 수정한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "게시글 수정 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @PutMapping("/{idx}")
+    public PostDTO updatePost(@PathVariable Long idx, @Valid @RequestBody PostEntity postEntity) {
+        if (postService.findOnePost(idx) == null) {
+            throw new TravelException(NOT_FOUND_POST, new Throwable());
+        } else {
+            return postService.updatePost(postEntity);
+        }
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : deletePost
+     * 2. ClassName  : PostController.java
+     * 3. Comment    : 게시글 삭제
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 12. 12.
+     * </pre>
+     */
+    @ApiOperation(value = "게시글 삭제", notes = "게시글 삭제한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "게시글 삭제 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @DeleteMapping("/{idx}")
+    public Long deletePost(@PathVariable Long idx) {
+        return postService.deletePost(idx);
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : togglePopular
+     * 2. ClassName  : PostController.java
+     * 3. Comment    : 인기 게시글 설정
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 12. 12.
+     * </pre>
+     */
+    @ApiOperation(value = "게시글 고정글 설정", notes = "인기 게시글 설정한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "인기 게시글 설정 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @PutMapping("/{idx}/toggle-popular")
+    public Boolean togglePopular(@PathVariable Long idx) {
+        return postService.togglePopular(idx);
+    }
+}
---
+++ src/test/java/com/travel/travel_project/api/post/PostControllerTest.java
@@ -0,0 +1,179 @@
+package com.travel.travel_project.api.post;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.travel.travel_project.domain.post.PostEntity;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.event.EventListener;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.persistence.EntityManager;
+
+import static com.travel.travel_project.common.StringUtil.getString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.test.context.TestConstructor.AutowireMode.ALL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+@TestConstructor(autowireMode = ALL)
+@RequiredArgsConstructor
+@AutoConfigureTestDatabase(replace= NONE)
+@DisplayName("게시글 Api Test")
+class PostControllerTest {
+
+    private MockMvc mockMvc;
+    private final WebApplicationContext wac;
+    private final ObjectMapper objectMapper;
+    private final EntityManager em;
+
+    @BeforeEach
+    @EventListener(ApplicationReadyEvent.class)
+    public void setup() {
+        this.mockMvc = webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysExpect(status().isOk())
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("게시글 조회 테스트")
+    void 게시글조회테스트() throws Exception {
+        mockMvc.perform(get("/api/post/lists").param("page", "1").param("size", "100"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.postList.length()", greaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("게시글 상세 조회 테스트")
+    void 게시글상세조회테스트() throws Exception {
+        mockMvc.perform(get("/api/post/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.idx").value(1L));
+    }
+
+    @Test
+    @DisplayName("게시글 등록 테스트")
+    void 게시글등록테스트() throws Exception {
+        PostEntity postEntity = PostEntity.builder()
+                .postTitle("게시글 테스트")
+                .postDescription("게시글 테스트")
+                .popular(false)
+                .viewCount(0)
+                .favoriteCount(0)
+                .visible("Y")
+                .build();
+
+        mockMvc.perform(post("/api/post")
+                        .contentType(APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(postEntity)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.postTitle").value("게시글 테스트"))
+                .andExpect(jsonPath("$.postDescription").value("게시글 테스트"));
+    }
+
+    @Test
+    @DisplayName("게시글 수정 테스트")
+    void 게시글수정테스트() throws Exception {
+        PostEntity postEntity = PostEntity.builder()
+                .postTitle("게시글 테스트")
+                .postDescription("게시글 테스트")
+                .popular(false)
+                .viewCount(0)
+                .favoriteCount(0)
+                .visible("Y")
+                .build();
+
+        em.persist(postEntity);
+
+        PostEntity updatePostEntity = PostEntity.builder()
+                .idx(postEntity.getIdx())
+                .postTitle("게시글 수정 테스트")
+                .postDescription("게시글 수정 테스트")
+                .popular(false)
+                .viewCount(0)
+                .favoriteCount(0)
+                .visible("Y")
+                .build();
+
+        mockMvc.perform(put("/api/post/{idx}", updatePostEntity.getIdx())
+                        .contentType(APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(updatePostEntity)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.postTitle").value("게시글 수정 테스트"))
+                .andExpect(jsonPath("$.postDescription").value("게시글 수정 테스트"));
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 테스트")
+    void 게시글삭제테스트() throws Exception {
+        PostEntity postEntity = PostEntity.builder()
+                .postTitle("게시글 테스트")
+                .postDescription("게시글 테스트")
+                .popular(false)
+                .viewCount(0)
+                .favoriteCount(0)
+                .visible("Y")
+                .build();
+
+        em.persist(postEntity);
+
+        mockMvc.perform(delete("/api/post/{idx}", postEntity.getIdx()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(content().string(getString(postEntity.getIdx())));
+    }
+
+    @Test
+    @DisplayName("인기 게시글 설정 테스트")
+    void 인기게시글설정테스트() throws Exception {
+        PostEntity postEntity = PostEntity.builder()
+                .postTitle("게시글 테스트")
+                .postDescription("게시글 테스트")
+                .popular(false)
+                .viewCount(0)
+                .favoriteCount(0)
+                .visible("Y")
+                .build();
+
+        em.persist(postEntity);
+
+        mockMvc.perform(put("/api/post/{idx}/toggle-popular", postEntity.getIdx()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(content().string(String.valueOf(true)));
+    }
+}
